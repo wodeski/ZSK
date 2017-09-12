c43b5a867b361536f2652f54012057c29670f6b6
wodeski <wddembik1@gmail.com>
2017-09-12 15:43:16 +0200

    Zajęcia 4 TD

---
+++ TD/js/funkcje.js
@@ -0,0 +1,39 @@
+/*function witaj(imie){
+    document.write("witaj" + imie);
+}
+
+
+var imie = " Adrian";
+
+witaj(imie);
+function poleProstokata(a,b){
+    var pole = a*b;
+    return pole;
+}
+var szerokosc = prompt("podaj szerokość");
+var dlugosc = prompt("podaj długość");
+document.write(poleProstokata(szerokosc, dlugosc));*/
+
+/*function poleObjetosc(szerokosc, dlugosc, wysokosc){
+    var pole = szerokosc*dlugosc;
+    var objetosc = pole*wysokosc;
+
+    var wynik = [pole, objetosc];
+    return wynik;
+
+}
+console.log(poleObjetosc(2,3,4));
+var pole = poleObjetosc(2,3,10)[0];
+var objetosc = poleObjetosc(2,3,10)[1];
+
+console.log(pole);
+
+var x = prompt("Podaj wartość", "0 to jest pole, 1 to jest objetosc");
+var dane = poleObjetosc(2,3,10)[x];
+console.log(dane);*/
+
+//napisz skrytp, który oblicza obwód, pole kola oraz objetosc stożka, dane uzytkownik podaje z klawiatury wykorzystaj obiekt math. uzytkownik wybiera za pomoca pola radio co chce obliczyc dane wprowadza w tekst dane wyswietl w bloku
+
+
+
+
